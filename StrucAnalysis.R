366824ed390b4e349c8c5f43ae2effb11e414703
brianalf-hub <70289096+brianalf-hub@users.noreply.github.com>
2020-09-18 13:19:00 -0600

    Create StrucAnalysis.R

---
+++ StrucAnalysis.R
@@ -0,0 +1,15 @@
+# Dissertation Chapter 3 codes
+# Structure in R
+
+#############################################################################
+# Installing packages
+
+install.packages(c("fields","RColorBrewer","mapplots"))
+source("http://bioconductor.org/biocLite.R")
+biocLite("LEA")
+
+install.packages("LEA_1.4.0_tar.gz", repos = NULL, type ="source")
+
+source("http://membres-timc.imag.fr/Olivier.Francois/Conversion.R")
+source("http://membres-timc.imag.fr/Olivier.Francois/POPSutilities.R")
+
